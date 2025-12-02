9d144a83149c303afd0c3c857b9ab72585578dbe
Domovenokkk <mezhuevmaksim3@gmail.com>
2025-12-02 16:54:08 +0300

    laba3

--- tasks/mityaeva_d_contrast_enhancement_histogram_stretching/mpi/include/ops_mpi.hpp
+++ tasks/mityaeva_d_contrast_enhancement_histogram_stretching/mpi/include/ops_mpi.hpp
@@ -13,7 +13,8 @@ class ContrastEnhancementMPI : public BaseTask {
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
     return ppc::task::TypeOfTask::kMPI;
   }
-  explicit ContrastEnhancementMPI(const InType &in);
+
+  explicit ContrastEnhancementMPI(const InType& in);
 
  private:
   bool ValidationImpl() override;
@@ -22,14 +23,17 @@ class ContrastEnhancementMPI : public BaseTask {
   bool PostProcessingImpl() override;
 };
 
-void FindGlobalMinMax(const std::vector<uint8_t> &local_pixels, unsigned char &global_min, unsigned char &global_max);
+void FindGlobalMinMax(const std::vector<uint8_t>& local_pixels,
+                      unsigned char& global_min,
+                      unsigned char& global_max);
 
-}
-
-std::vector<uint8_t> ProcessLocalPixels(const std::vector<uint8_t> &local_pixels, uint8_t global_min,
+std::vector<uint8_t> ProcessLocalPixels(const std::vector<uint8_t>& local_pixels,
+                                        uint8_t global_min,
                                         uint8_t global_max);
 
-void GatherResults(int rank, int size, const std::vector<uint8_t> &local_result, int width, int height,
-                   std::vector<uint8_t> &final_output);
+void GatherResults(int rank, int size,
+                   const std::vector<uint8_t>& local_result,
+                   int width, int height,
+                   std::vector<uint8_t>& final_output);
 
 }  // namespace mityaeva_d_contrast_enhancement_histogram_stretching
